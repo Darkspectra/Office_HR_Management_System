e93e6fc158753e714e4aeae1bd11f5ace2af91f0
Farhan Tanvir Ahmed <57720288+Darkspectra@users.noreply.github.com>
2023-04-10 13:04:45 +0600

    Add files via upload

---
+++ Sprint_5/database/migrations/2023_02_21_113644_project.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project' , function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ProjectName');
+            $table->string('ProjectID')->unique();;
+            $table->string('Duration');
+            $table->string('Name');
+            $table->string('Email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project');
+    }
+};
